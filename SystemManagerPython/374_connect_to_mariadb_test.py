2c1df0bf0cf33c6468ec24a8a0feae7711edadea
zingsman <11946769@qq.com>
2018-08-31 15:46:41 +0800

    pc端更新

--- SystemManagerPython/374_connect_to_mariadb_test.py
+++ SystemManagerPython/374_connect_to_mariadb_test.py
@@ -1,7 +1,6 @@
 import os
 import datetime
 import pymysql as db
-import time
 def get_conn(**kwargs):
     mysqldb=db.connect(host=kwargs.get('host', 'localhost'),user=kwargs.get('user'),
                passwd=kwargs.get('passwd'),
@@ -9,23 +8,16 @@ def get_conn(**kwargs):
                db=kwargs.get('db'))
     return mysqldb
 def main():
-    conn = get_conn(host='127.0.0.1',
-                    user='app',
-                    passwd='app_!QAZxsw2',
-                    port=3306,db='pk')
+    conn = get_conn(host='127.0.0.1',user='app',passwd='xxxx',port=3306,db='pk')
     cur = conn.cursor()
-    cur.execute('SELECT * FROM depute_trans')
+    cur.execute('SELECT trans_date_time,acct_name,trans_amount,inst_code FROM depute_trans')
     all_data=cur.fetchall()
     cur.close()
     conn.close()
-#    for i in all_data:
-#        print(i[0])
-#        print(time.strftime('%Y%m%d',time.localtime()))
     with open('C:\\Users\\ke.peng\\Desktop\\test\\mysql_data.txt','w') as f:
         for data in all_data:
-            for i in data:
-                f.write(str(i) + '|')
-            f.write('\n')
+            print(data)
+            f.write(str(data) + '\n')
         print('Data write to "mysql_data.txt" is over!')
 if __name__ == '__main__':
     main()
--- SystemManagerPython/tmp.py
+++ SystemManagerPython/tmp.py
@@ -1,18 +1,3 @@
-import  os,openpyxl,glob
-
-def main():
-    dirname='C:\\Users\\ke.peng\\Desktop\\test\\tmp'
-    files=glob.glob(os.path.join(dirname,'*.xlsx'))
-    print(files)
-    wb = openpyxl.load_workbook(files[0])
-    ws = wb.active
-    ws.title = 'All result'
-    for i in files[1:]:
-        workbook = openpyxl.load_workbook(i)
-        sheet = workbook.active
-        for row in sheet.iter_rows():
-            values = [cell.value for cell in row]
-            ws.append(values)
-    wb.save(dirname + '\\new.xlsx')
-if __name__ == '__main__':
-    main()
+import configparser
+cf=configparser.ConfigParser()
+cf.read('..\\my_config')
